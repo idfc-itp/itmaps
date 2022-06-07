134338bf0269f5666d34493e27e1ea6e4f7bfd3b
ifernandeznv <ifernandeznv@gmail.com>
2022-06-06 19:06:08 -0600

    Agregar Zoom al mapa

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const nombreCache = 'itm-v1';
+const nombreCache = 'itm-v5';
 const archivos = [
     './index.html',
     './css/styles.css',
@@ -8,7 +8,18 @@ const archivos = [
     './js/mixitup.min.js',
     './js/scrollreveal.min.js',
     './img/header.PNG',
-    './img/mapa.jpg',
+    './img/rutas/mapa.jpg',
+    './img/rutas/Administrativo.jpg',
+    './img/rutas/Audiovisual.jpg',
+    './img/rutas/Aulas C.jpg',
+    './img/rutas/Aulas D.jpg',
+    './img/rutas/Centor de Información.jpg',
+    './img/rutas/Dirección.jpg',
+    './img/rutas/División de Estudios Profesionales.jpg',
+    './img/rutas/Laboratorio de Electromecánica.jpg',
+    './img/rutas/Laboratorios de Informática.jpg',
+    './img/rutas/Química General.jpg',
+    './img/rutas/Servicios escolares.jpg',
     './manifest.json'
 ];
 self.addEventListener('install', e =>{
